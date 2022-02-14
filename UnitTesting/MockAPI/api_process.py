f87355f56220f138b56ba468980f09d44f8b7bdb
james <j.alsop99@hotmail.com>
2022-02-14 18:13:52 +0000

    mock web api testing

--- UnitTesting/MockAPI/api_process.py
+++ UnitTesting/MockAPI/api_process.py
@@ -1,4 +1,25 @@
 
+import json
 import requests
 
+GET_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"
 
+POST_URL = ""
+
+def get_from_api():
+    
+    response = requests.get(GET_URL)
+
+    return response.text
+
+def post_to_api(data):
+
+    response = requests.post(POST_URL, json=data)
+
+
+def main():
+    text = get_from_api()
+    print(text)
+
+if __name__ == "__main__":
+    main()
--- UnitTesting/MockAPI/test_api.py
+++ UnitTesting/MockAPI/test_api.py
@@ -2,5 +2,27 @@
 import unittest
 from unittest.mock import patch
 
+import api_process
+
+class MockResponse():
+
+    def __init__(self):
+        pass
+
+class TestAPI(unittest.TestCase):
+    
+    def test_get(self):
+        with patch("api_process.requests.get") as mock_get:
+            
+            response = MockResponse()
+            response.text = "something that isn't latin"
+            
+            mock_get.return_value = response
+
+            text = api_process.get_from_api()
+
+            print(text)
 
 
+if __name__ == "__main__":
+    unittest.main()
