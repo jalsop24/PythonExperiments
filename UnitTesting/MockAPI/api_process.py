63b9b62c90a226fffdb403c2fd92bb64e72772ac
james <j.alsop99@hotmail.com>
2022-02-15 14:55:42 +0000

    Finished post method

--- UnitTesting/MockAPI/api_process.py
+++ UnitTesting/MockAPI/api_process.py
@@ -4,22 +4,26 @@ import requests
 
 GET_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"
 
-POST_URL = ""
+POST_URL = "https://www.abc.com"
 
-def get_from_api():
+def get_from_api(params=None):
     
-    response = requests.get(GET_URL)
+    response = requests.get(GET_URL, params=params)
 
-    return response.text
+    return response
 
 def post_to_api(data):
 
     response = requests.post(POST_URL, json=data)
 
+    return response
 
 def main():
     text = get_from_api()
     print(text)
+
+    r = post_to_api({"a": 1})
+    print(r)
 
 if __name__ == "__main__":
     main()
