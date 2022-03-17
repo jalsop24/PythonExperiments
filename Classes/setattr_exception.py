6900aa6e17088034124f6584e3fa637cbc78c3ed
james <j.alsop99@hotmail.com>
2022-03-17 16:49:13 +0000

    Updated value

--- Classes/setattr_exception.py
+++ Classes/setattr_exception.py
@@ -6,7 +6,7 @@ class ANewClass():
 
 try:
     an_instance = ANewClass()
-    setattr(an_instance, ATTR_NAME, "A value")
+    setattr(an_instance, ATTR_NAME, "Another value, maybe 2. I hope no one else changed this.")
 except Exception as err:
     print(err)
 else:
